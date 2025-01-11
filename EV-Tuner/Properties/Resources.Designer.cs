c1cda018116022d68c18c09035052c0dcb7b10a7
MohakB <Iammohakb@gmail.com>
2025-01-10 20:49:57 -0800

    Added Font in Resources

--- EV-Tuner/Properties/Resources.Designer.cs
+++ EV-Tuner/Properties/Resources.Designer.cs
@@ -261,6 +261,16 @@ namespace EV_Tuner.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Byte[].
+        /// </summary>
+        internal static byte[] Vogue {
+            get {
+                object obj = ResourceManager.GetObject("Vogue", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
         internal static System.Drawing.Bitmap white_texture {
